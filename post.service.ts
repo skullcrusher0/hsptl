834efe7b082ac7ac64fdf9b29abbab51dd25db58
skullcrusher0 <48072661+skullcrusher0@users.noreply.github.com>
2019-03-01 13:19:32 +0530

    Add files via upload

---
+++ post.service.ts
@@ -0,0 +1,178 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Post ,Event } from "../services/user";
+import { AuthService } from 'src/app/services/auth.service';
+import { AngularFireAuth } from "@angular/fire/auth";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  postCollection: AngularFirestoreCollection<Post>;
+  eventCollection: AngularFirestoreCollection<Event>;
+
+  public posts: Post[];
+  public events:Event[];
+  
+  userId: string;
+  userName: string;
+
+
+  constructor(public afs: AngularFirestore, public authService: AuthService, public afAuth: AngularFireAuth) {
+    this.setUserId();
+  }
+  setUserId(){
+    this.authService.afAuth.auth.onAuthStateChanged(s => { this.userId = s.uid, this.userName = s.displayName })
+  }
+
+  getPosts() {
+
+    setTimeout(() => {
+      console.log(this.userId);
+      this.postCollection = this.afs.collection('posts', ref => ref.where('author_id', '==', this.userId));
+      this.postCollection.snapshotChanges().subscribe((doc) => {
+        this.posts = doc.map(data => {
+          return data.payload.doc.data();
+        });
+      })
+    }, 3000)
+  }
+
+  
+  getEvents() {
+
+    setTimeout(() => {
+      console.log(this.userId);
+      this.eventCollection = this.afs.collection('events');
+      this.eventCollection.snapshotChanges().subscribe((doc) => {
+        this.events = doc.map(data => {
+          return data.payload.doc.data();
+        });
+      })
+    }, 3000)
+
+  }
+
+
+
+
+  createPost(inputData:any) {
+    const data:Post ={ 
+      author_id: this.userId,
+      title:inputData.title,
+      content: inputData.content,
+    }
+    this.afs.collection('posts').ref.add(data)
+    .then(()=>console.log("posted SuccessFlly"))
+    .catch((error)=>console.log(error.message))
+  }
+
+  createEvent(inputData:any){
+    const data : Event={ 
+     // author_id:this.userId,
+      link:inputData.link,
+      title:inputData.title,
+      content:inputData.content
+    }
+    this.afs.collection('events').ref.add(data).then(()=>console.log("event posted SuccessFlly"))
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// getPosts() {
+  //   this.authService.afAuth.auth.onAuthStateChanged(user => {
+  //     if (user) {
+  //       this.userId = user.uid;
+  //       console.log(this.userId);
+  //       this.postCollection = this.afs.collection('posts', ref => ref.where('author_id', '==', this.userId));
+  //       return this.postCollection.snapshotChanges().pipe(map(action => {
+  //         this.postData = action;
+  //         return action.map(a => {
+  //           this.postData = a.payload.doc.data() as Post;
+
+  //           const id = a.payload.doc.id;
+  //           return { id, ...data };
+  //         })
+  //       }))
+  //     }
+  //   });
+
+  //   // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   // getPosts(){
+//   //   this.authService.afAuth.auth.onAuthStateChanged(user => {
+//   //     if (user) {
+//   //       this.userId = user.uid;
+//   //       console.log(this.userId);
+//   //       this.postCollection =  this.afs.collection('posts', ref =>
+//   //       {
+//   //         return ref.where('author_id', '==',this.userId);
+//   //       })
+//   //       
+//   //       this.postCollection.valueChanges().subscribe(docs=>{
+//   //         this.postData =  docs;
+//   //       })
+//   //     }
+//   //   });
+//   //   //console.log(this.posts);
+//   // }
